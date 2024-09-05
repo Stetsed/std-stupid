e44482640f8c369db2a9054b923e484f4620ba38
Stetsed <stetsed@selfhostable.net>
2024-09-05 23:12:37 +0200

    Redone HTTP parsing
    - Redid the HTTP request parsing to be more efficient(rougly 40k v. 60k
    requests per second , and using the sample.html test file a bandwith of 12Gb from a previous 7Gb)
    - Removed unwraps from parsing to prevent malicious packet from crashing
      the program.

--- http_stupid/src/http_parser.rs
+++ http_stupid/src/http_parser.rs
@@ -1,8 +1,7 @@
-use std::{collections::HashMap, str};
-
 use crate::*;
 use errors_stupid::StdStupidError;
 use standard_stupid::findSubStringWithBytes;
+use std::{collections::HashMap, str};
 
 /// Takes an argument of `&[u8]` with the data contained being that from a buffered reader on a
 /// TCPListerner and returns the data contained within including the httpVersion used, the type of request that was recieved, the path that was requested,
@@ -11,94 +10,103 @@ use standard_stupid::findSubStringWithBytes;
 pub fn parse_http_connection(
     mut connection_data_raw: &[u8],
 ) -> Result<ParseReturnData, StdStupidError> {
-
-    for i in connection_data_raw.lines(){
-        println!("new line");
-        println!("{:?}", i.unwrap());
-    }
-
-    let mut connection_data = connection_data_raw.to_vec();
-    // Find the / to find the method being used
-    let method_location = findSubStringWithBytes(connection_data.as_slice(), b"/")?;
-
-    let request_type_raw = str::from_utf8(&connection_data[0..method_location as usize - 1])?;
-
-    let http_request_type_given: HttpRequestType = parse_http_request_type(request_type_raw);
-
-    connection_data.drain(0..method_location as usize);
-
-    // Find the H to know the entire path
-    let request_path_location = findSubStringWithBytes(connection_data.as_slice(), &[0x48])?;
-
-    let request_path_given: String =
-        str::from_utf8(&connection_data[0..request_path_location as usize - 1])?.to_string();
-
-    connection_data.drain(0..request_path_location as usize);
-
-    // Find the first CLRF
-    let http_location = findSubStringWithBytes(connection_data.as_slice(), &[0x0a])?;
-
-    let http_version_given: f32 =
-        str::from_utf8(&connection_data[5..http_location as usize - 1])?.parse::<f32>()?;
-
     let mut header_hash_map: HashMap<String, String> = HashMap::new();
+    let mut http_version_given: Option<f32> = None;
+    let mut http_request_type_given: Option<HttpRequestType> = None;
+    let mut http_path_given: Option<String> = None;
+    for (e, i) in connection_data_raw.lines().enumerate() {
+        let unwrapped = i?;
 
-    connection_data.drain(0..http_location as usize + 1);
+        if e == 0 {
+            let mut rest: &[u8];
+            let http: &[u8];
+            let path: &[u8];
+            let version: &[u8];
+            // Find the slash and get the HTTP request type
+            (http, rest) = unwrapped
+                .as_bytes()
+                .split_at_checked(
+                    unwrapped
+                        .find('/')
+                        .ok_or_else(|| HttpServerError::new("Failed to find /"))?,
+                )
+                .ok_or_else(|| {
+                    HttpServerError::new("Failed to split string... should be impossible")
+                })?;
+            http_request_type_given = Some(parse_http_request_type(str::from_utf8(http).unwrap()));
+            (path, rest) = rest
+                .split_at_checked(
+                    rest.windows(1)
+                        .position(|c| matches!(c, b"H"))
+                        .ok_or_else(|| HttpServerError::new("Failed to find H"))?,
+                )
+                .ok_or_else(|| {
+                    HttpServerError::new("Failed to split string... should be impossible")
+                })?;
 
-    while connection_data.len() > 2 {
-        let header_clrf_location =
-            findSubStringWithBytes(connection_data.as_slice(), &[0x0a]).unwrap_or(0);
+            http_path_given = Some(str::from_utf8(path)?.trim().to_string());
 
-        let header_clrf_location_get: u32;
-        let header_clrf_location_drain: usize;
+            (_, version) = rest
+                .split_at_checked(
+                    rest.windows(1)
+                        .position(|c| matches!(c, b"/"))
+                        .ok_or_else(|| HttpServerError::new("Failed to find /"))?
+                        + 1,
+                )
+                .ok_or_else(|| {
+                    HttpServerError::new("Failed to split string... should be impossible")
+                })?;
 
-        if header_clrf_location == 0 {
-            header_clrf_location_get = connection_data.len() as u32;
-            header_clrf_location_drain = connection_data.len();
+            http_version_given = Some(str::from_utf8(version)?.parse::<f32>()?);
+        } else if unwrapped.is_empty() {
+            break;
         } else {
-            header_clrf_location_get = header_clrf_location - 1;
-            header_clrf_location_drain = (header_clrf_location + 1) as usize;
+            let header: Vec<&str> = unwrapped.split(":").collect();
+
+            let http_header_name = header[0].trim().to_string();
+            let http_header_content = header[1..].concat().trim().to_string();
+
+            header_hash_map.insert(http_header_name, http_header_content);
         }
-        let header_supplied = connection_data[0..header_clrf_location_get as usize].to_vec();
-
-        let header_double_period_location =
-            findSubStringWithBytes(header_supplied.as_slice(), b":").unwrap();
-
-        let header_name =
-            str::from_utf8(&header_supplied[0..header_double_period_location as usize])?
-                .to_string();
-
-        let header_content = str::from_utf8(
-            &header_supplied
-                [header_double_period_location as usize + 2..header_clrf_location as usize - 1],
-        )?
-        .to_string();
-        header_hash_map.insert(header_name, header_content);
-
-        connection_data.drain(0..header_clrf_location_drain);
+    }
+    #[cfg(debug_assertions)]
+    {
+        println!(
+            "HTTP Request path given by new functiopn was: {:?}",
+            http_path_given
+        );
+        println!(
+            "HTTP request type found by new function was: {:?}",
+            http_request_type_given
+        );
+        println!(
+            "HTTP Version given by new function was: {:?}",
+            http_version_given
+        );
+        for (header, content) in &header_hash_map {
+            println!("Header: {header} : {content}");
+        }
     }
 
-    drop(connection_data);
-    #[cfg(debug_assertions)]
-    println!("HTTP Version found is: {:?}", http_version_given);
-    #[cfg(debug_assertions)]
-    println!("Request path found is: {:?}", request_path_given);
-    #[cfg(debug_assertions)]
-    println!("Request type found is: {:?}", http_request_type_given);
-    #[cfg(debug_assertions)]
-    for (header, content) in &header_hash_map {
-        println!("Header: {header} : {content}");
+    if http_version_given.is_some()
+        && http_request_type_given.is_some()
+        && http_path_given.is_some()
+    {
+        Ok(ParseReturnData {
+            httpVersion: http_version_given.unwrap(),
+            HttpRequestType: http_request_type_given.unwrap(),
+            requestPath: http_path_given.unwrap(),
+            headers: header_hash_map,
+        })
+    } else {
+        Err(StdStupidError::HttpServer(HttpServerError {
+            source: "One of the connection points was invalid".into(),
+        }))
     }
-    Ok(ParseReturnData {
-        httpVersion: http_version_given,
-        HttpRequestType: http_request_type_given,
-        requestPath: request_path_given,
-        headers: header_hash_map,
-    })
 }
 
 fn parse_http_request_type<T: AsRef<str>>(to_parse: T) -> HttpRequestType {
-    match to_parse.as_ref() {
+    match to_parse.as_ref().trim() {
         "GET" => HttpRequestType::GET,
         "POST" => HttpRequestType::POST,
         "HEAD" => HttpRequestType::HEAD,
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 #![feature(addr_parse_ascii, ip, tcplistener_into_incoming)]
 #![allow(non_snake_case)]
 
+use errors_stupid::*;
 use http_stupid::http_struct::*;
 use http_stupid::HttpServer;
-use errors_stupid::*;
 
 fn main() -> Result<(), StdStupidError> {
     let IpAddressToUse: String = "0.0.0.0".to_string();
     let portTouse: u16 = 9182;
 
     let mut HttpServer = HttpServer::new(
-        server_function::Debug,
+        server_function::ServeFile,
         Some(IpAddressToUse),
         Some(portTouse),
     )?;
